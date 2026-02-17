8fd3255f3bc6698738d6142dc7fea67cb063a79e
EKnibbe <E.Knibbe@tudelft.nl>
2026-02-17 15:15:06 +0100

    Add example R file

---
+++ example.R
@@ -0,0 +1,19 @@
+library(readxl)
+
+file_path <- "..\\TT_Template_try1_wip.xlsx" 
+sheets <- excel_sheets(file_path)
+data_list <- lapply(sheets, function(x) read_excel(file_path, sheet = x))
+names(data_list) <- sheets
+View(data_list)
+
+View(data_list[["Notes"]])
+View(data_list[["Experimental_parameters"]])
+View(data_list[["Planning_preculture"]])
+View(data_list[["CellCount_Viability"]])
+View(data_list[["Cone_viability"]])
+View(data_list[["Attenuation"]])
+View(data_list[["pH"]])
+View(data_list[["HPLC_raw"]])
+View(data_list[["HPLC"]])
+View(data_list[["GC"]])
+
