75c6d36e4bae1aa2d5e4eb2224ef141c8a462513
EKnibbe <E.Knibbe@tudelft.nl>
2026-02-19 13:52:25 +0100

    Update example.R

--- example.R
+++ example.R
@@ -1,6 +1,6 @@
 library(readxl)
 
-file_path <- "..\\TT_Template_try1_wip.xlsx" 
+file_path <- "..\\TT_Template_try2.xlsx" 
 sheets <- excel_sheets(file_path)
 data_list <- lapply(sheets, function(x) read_excel(file_path, sheet = x))
 names(data_list) <- sheets
@@ -13,9 +13,9 @@ View(data_list[["CellCount_Viability"]])
 View(data_list[["Cone_viability"]])
 View(data_list[["Attenuation"]])
 View(data_list[["pH"]])
-View(data_list[["HPLC_raw"]])
 View(data_list[["HPLC"]])
-View(data_list[["GC"]])
+View(data_list[["GC_esters"]])
+View(data_list[["GC_ketones"]])
 
 #this is a change
 # mvdb changed this line
