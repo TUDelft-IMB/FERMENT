bb6b5cb39880f143b51e864ecb969b16280ed2aa
edvinskis <E.Dvinskis@tudelft.nl>
2026-03-31 15:19:47 +0200

    Remove path

--- global.R
+++ global.R
@@ -33,7 +33,7 @@ library(purrr)
 # -----------------------------------------------------------------------------
 
 # Change path accordingly
-EXCEL_DIR <- Sys.getenv("TT_EXCEL_DIR", "/Users/edvinskis/Desktop/shinytest")
+EXCEL_DIR <- Sys.getenv("TT_EXCEL_DIR", "/PATH/PATH")
 
 # -----------------------------------------------------------------------------
 # LOAD DATA
