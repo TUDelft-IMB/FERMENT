4bbe9b4eb9a169627c25122dc65e1a1191a29693
edvinskis <E.Dvinskis@tudelft.nl>
2026-06-25 16:11:40 +0200

    Add comment

--- global.R
+++ global.R
@@ -74,7 +74,7 @@ if (Sys.getenv("TT_EXCEL_DIR") == "") {
 
 # The fallback "PATH/PATH" will cause an informative error if the variable
 # is not set, rather than rendering an empty dashboard later.
-if (EXCEL_DIR == "PATH/PATH") {
+if (EXCEL_DIR == "PATH/PATH") {     # Do not change this PATH/PATH
   stop("Environment variable TT_EXCEL_DIR is not set.")
 }
 
