86b205394af39d08a390321edf7188a06333deb9
edvinskis <E.Dvinskis@tudelft.nl>
2026-07-03 10:03:38 +0200

    Add GC Ratio tabPanel to UI

--- ui.R
+++ ui.R
@@ -493,6 +493,12 @@ ui <- navbarPage(
             )
           ),
           tabPanel(
+            "GC Ratio",
+            fluidRow(
+              column(12, spinner(plotlyOutput("avg_gc_ratio_plot", height = "450px")))
+            )
+          ),
+          tabPanel(
             "Attenuation",
             fluidRow(
               column(12, spinner(plotlyOutput("avgAttenuationPlot", height = "450px")))
