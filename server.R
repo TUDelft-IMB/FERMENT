ed10fd5a6468c2d573dcce5e03a334a3191683d5
edvinskis <E.Dvinskis@tudelft.nl>
2026-07-03 14:55:50 +0200

    Revert "Merge branch '84-add-zoom-inzoom-out-button-for-the-whole-app' of https://github.com/TUDelft-IMB/FERMENT into 84-add-zoom-inzoom-out-button-for-the-whole-app"
    This reverts commit 2401f0aafc0688719b7cdc0e24697a525e4ac55f, reversing
    changes made to a19dc0badc11f50989c22c6c59ae7df7858f2566.

--- server.R
+++ server.R
@@ -664,12 +664,6 @@ server <- function(input, output, session) {
     req(avg_data_list())
     ggplotly(plot_avg_diketones(avg_data_list(), avg_exp_labels()))
   })
-  
-  # GC Ratio plot
-  output$avg_gc_ratio_plot <- renderPlotly({
-    req(avg_data_list())
-    ggplotly(plot_avg_gc_ratio_bar(avg_data_list(), avg_exp_labels()))
-  })
 
   # Attenuation, pH, Cell Count, Viability: one line per experiment
   output$avgAttenuationPlot <- renderPlotly({
